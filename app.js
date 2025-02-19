403c22fdc853c402a7719c6dd8e1a1b0ec7fd6b2
Maallez <maicon.sallez@gmail.com>
2025-02-18 21:26:21 -0300

    Adicionado função de reiniciar o jogo. Enviar o formulario com enter. E modificação no inicio do código.

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 let amigos = [];
+let listagem = document.getElementById("listaAmigos");
+let addLi = document.createElement("li");
+
 //função para adicionar amigos
 //verificar campo vazio
 //adicionar amigos na lista
@@ -6,7 +9,25 @@ function adicionarAmigo(){
     let inAmigo = document.getElementById("amigo").value.trim();
 if (inAmigo === ""){
     alert ("Por favor, insira um nome.");
+    return;
 }
-amigos.push(inAmigo);
-document.getElementById("amigo").value = "";
+    amigos.push(inAmigo);
+    document.getElementById("amigo").value = "";
+    addLi.textContent = inAmigo;
+    listagem.appendChild(addLi);
+}
+
+// Enviar o formulario com Enter
+let input = document.getElementById("amigo");
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("enter-button").click();
+  }
+});
+
+function reiniciarJogo(){
+    amigos = [];
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
 }
