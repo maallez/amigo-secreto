0c7b97949ed18219380babb5530785b8089c4453
Maallez <maicon.sallez@gmail.com>
2025-02-18 22:50:26 -0300

    Melhorar a função de reiniciar o jogo, adicionar a função de sortear os nomes. Corrigir a posição dos lets "listagem" e "addLi".

--- app.js
+++ app.js
@@ -1,33 +1,62 @@
 let amigos = [];
-let listagem = document.getElementById("listaAmigos");
-let addLi = document.createElement("li");
-
 //função para adicionar amigos
-//verificar campo vazio
+//verificar campo vazio'
 //adicionar amigos na lista
-function adicionarAmigo(){
-    let inAmigo = document.getElementById("amigo").value.trim();
-if (inAmigo === ""){
-    alert ("Por favor, insira um nome.");
+function adicionarAmigo() {
+  let inAmigo = document.getElementById("amigo").value.trim();
+  if (inAmigo === "") {
+    alert("Por favor, insira um nome.");
     return;
-}
-    amigos.push(inAmigo);
-    document.getElementById("amigo").value = "";
-    addLi.textContent = inAmigo;
-    listagem.appendChild(addLi);
+  }
+  let listagem = document.getElementById("listaAmigos");
+
+  let addLi = document.createElement("li");
+
+  amigos.push(inAmigo);
+
+  document.getElementById("amigo").value = "";
+
+  addLi.textContent = inAmigo;
+
+  listagem.appendChild(addLi);
 }
 
-// Enviar o formulario com Enter
+//enviar o input com Enter
 let input = document.getElementById("amigo");
-input.addEventListener("keypress", function(event) {
+
+input.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
     document.getElementById("enter-button").click();
   }
 });
 
-function reiniciarJogo(){
-    amigos = [];
-    let listaAmigos = document.getElementById("listaAmigos");
-    listaAmigos.innerHTML = "";
+//função responsavel por reiniciar o jogo
+function reiniciarJogo() {
+  amigos = [];
+
+  let listaAmigos = document.getElementById("listaAmigos");
+  listaAmigos.innerHTML = "";
+  alert("Lista de sorteados foi zerada.");
+
+  let listaDeSorteados = document.getElementById("resultado");
+  listaDeSorteados.innerHTML = "";
+}
+
+//função responsavel pelo sorteio dos nomes
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("Não há nomes para sortear.");
+    return;
+  }
+  let sorteio = Math.floor(Math.random() * amigos.length);
+
+  let nomeSorteado = amigos[sorteio];
+
+  let resultado = document.getElementById("resultado");
+  resultado.innerHTML = "";
+
+  let amigoSorteado = document.createElement("li");
+  amigoSorteado.textContent = `O amigo sorteado foi ${nomeSorteado}!`;
+  resultado.appendChild(amigoSorteado);
 }
